4fa3728525139b6dec867e0d77357cd15a4dbee3
motivve <115065824+motivve@users.noreply.github.com>
2024-01-15 23:29:15 +0100

    Create script.js

---
+++ projecteuler/script.js
@@ -0,0 +1,7 @@
+document.getElementById('showPopup').addEventListener('click', function() {
+    document.getElementById('codePopup').style.display = 'block';
+});
+
+document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('codePopup').style.display = 'none';
+});
