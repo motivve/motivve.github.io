b71b630d6a567437681f488858f1b75748492906
motivve <115065824+motivve@users.noreply.github.com>
2024-01-16 00:24:45 +0100

    Update script.js

--- projecteuler/script.js
+++ projecteuler/script.js
@@ -1,7 +1,19 @@
-document.getElementById('showPopup').addEventListener('click', function() {
-    document.getElementById('codePopup').style.display = 'block';
+document.getElementById('showPopup1').addEventListener('click', function() {
+    // Show the popup for problem 1
+    showPopup('codePopup1');
 });
 
-document.getElementById('closePopup').addEventListener('click', function() {
-    document.getElementById('codePopup').style.display = 'none';
+document.getElementById('closePopup1').addEventListener('click', function() {
+    // Close the popup for problem 1
+    closePopup('codePopup1');
+});
+
+document.getElementById('showPopup2').addEventListener('click', function() {
+    // Show the popup for problem 2
+    showPopup('codePopup2');
+});
+
+document.getElementById('closePopup2').addEventListener('click', function() {
+    // Close the popup for problem 2
+    closePopup('codePopup2');
 });
