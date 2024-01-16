51b15fe69e1c768620cee0232bb548fcf684753c
motivve <115065824+motivve@users.noreply.github.com>
2024-01-16 18:56:29 +0100

    Update script.js

--- projecteuler/script.js
+++ projecteuler/script.js
@@ -3,11 +3,11 @@ for (let i = 1; i <= 30; i++) {
 }
 
 function createEventListeners(index) {
-    document.getElementById('showPopup' + index).addEventListener('click', function() {
+    document.querySelector('#showPopup' + index).addEventListener('click', function() {
         showPopup('codePopup' + index);
     });
 
-    document.getElementById('closePopup' + index).addEventListener('click', function() {
+    document.querySelector('#closePopup' + index).addEventListener('click', function() {
         closePopup('codePopup' + index);
     });
 }
