4f9ca30c024d973356f20cd372aea81568db05ca
motivve <115065824+motivve@users.noreply.github.com>
2024-01-16 18:53:53 +0100

    Update script.js

--- projecteuler/script.js
+++ projecteuler/script.js
@@ -1,12 +1,13 @@
 for (let i = 1; i <= 30; i++) {
-    // Using IIFE to capture the current value of i
-    (function(index) {
-        document.getElementById('showPopup' + index).addEventListener('click', function() {
-            showPopup('codePopup' + index);
-        });
+    createEventListeners(i);
+}
 
-        document.getElementById('closePopup' + index).addEventListener('click', function() {
-            closePopup('codePopup' + index);
-        });
-    })(i);
+function createEventListeners(index) {
+    document.getElementById('showPopup' + index).addEventListener('click', function() {
+        showPopup('codePopup' + index);
+    });
+
+    document.getElementById('closePopup' + index).addEventListener('click', function() {
+        closePopup('codePopup' + index);
+    });
 }
